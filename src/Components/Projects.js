6abbeef7a23e04bad0737dc1ad2fe1690bdc12cd
Rishi Karri <rishikarri1@gmail.com>
2017-03-24 14:03:48 -0400

    added a nice clean line between projects rather than an hr

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,53 +10,79 @@ class Project extends Component{
 				<div className='row kill-margin'>
 
 					<div className='col-xs-12 col-sm-5 col-sm-offset-1'>
-						<hr className='heading-spacer'/>
+						
 
-						<div className='section-heading'>
-							<h2>COMING SOON: Village Savior</h2>
-							<p>Monsters and thieves are about to attack your village. Stave them off as long as you can in order to give the villagers time to escape! Be very careful! If your health falls to 0, you must retreat!</p>
-							<p> HTML & CSS, JavaScript (code is intentionally written without frameworks)</p>
+						<div id='atlanta-vibes-div' className='section-heading project-image-div'>
+							<h2>AtlantaVibes</h2>
+							<p>AtlantaVibes is a full-stack application that aggregates data from the top music festivals, in Atlanta, into one easy to use website. AtlantaVibes lets users view, rate and comment on their favorite festivals in Atlanta.</p>
+							<p> HTML & CSS, Bootstrap, Sass, Javascript, jQuery, React.js, Redux, Node.js, Express.js, MySql, AWS</p>
 							{/*Link to youtube video */}
 							<div className='repo-demo-links'>
-								<a className='margin-right' target="_blank" href='https://www.youtube.com/watch?v=kVRCTKB7NRo&feature=youtu.be'><button className='btn btn-primary'> Demo </button></a>
-								<a target="_blank" href='https://github.com/rishikarri/village-savior'><button className='btn btn-primary'> Repository</button></a>
+								<a className='margin-right' target="_blank" href='https://www.youtube.com/watch?v=u4IcZspgeHc&feature=youtu.be'><button className='btn btn-primary'> Demo </button></a>
+								<a target="_blank" href='https://github.com/rishikarri/AtlantaVibes'><button className='btn btn-primary'>Repository</button></a>
 							</div>
 						</div>
 					</div>
 
 					<div className='col-xs-12 col-sm-5'>						
-						<img className='project-image margin-above' src='../../Images/Projects-Page/survivor-screenshot.png' />
+						<img className='project-image margin-above' src='../../Images/Projects-Page/AtlantaVibesHomePage.png' />
 					</div>
 
 
 				</div>
 
+				<div className='col-xs-12 col-sm-10 col-sm-offset-1 spacerDivBetweenProjectImages'> </div>
 				<div className='row kill-margin margin-above-heavy'>
 
 					<div className='col-xs-12 hide-word-worm col-sm-5 col-sm-offset-0'>
 						<img className='project-image-word-worm margin-above' src='../../Images/Projects-Page/BookWorm.png' />
 					</div>
 
-					<div className='col-xs-12 col-sm-5 col-sm-offset-1'>						
-						
-
-						<hr className='heading-spacer'/>
+					<div className='col-xs-12 col-sm-5 col-sm-offset-1 project-image-div'>																	
 
 						<div className='section-heading'>
-							<h2>COMING SOON: WordWorms</h2>
+							<h2>WordWorms</h2>
 
 							<p>A word learning app. Users are able to take vocabulary quizzes, look up words, and much more!</p>
 							<p> HTML & CSS, Bootstrap, React.js & React-Router, JavaScript (ES-6), jQuery</p>
 
 							<div className='repo-demo-links'>
 								<a className='margin-right' target="_blank" href='https://github.com/CaseyTM/WordWorms/blob/master/src/Images/wordworms_1.gif'><button className='btn btn-primary'> Demo </button></a>
-								<a target="_blank" href='https://github.com/CaseyTM/WordWorms'><button className='btn btn-primary'> Repository</button></a>
+								<a target="_blank" href='https://github.com/CaseyTM/WordWorms'><button className='btn btn-primary'>Repository</button></a>
 							</div>							
 						</div>						
 					</div>
 
+					
+					
+
 					<div className='col-xs-12 show-word-worm col-sm-5 col-sm-offset-1'>
 						<img className='project-image-word-worm margin-above' src='../../Images/Projects-Page/BookWorm.png' />
+					</div>
+
+
+				</div>
+
+				<div className='col-xs-12 col-sm-10 col-sm-offset-1 spacerDivBetweenProjectImages'> </div>
+				<div className='row kill-margin margin-above-heavy'>
+
+					<div className='col-xs-12 col-sm-5 col-sm-offset-1'>
+						
+
+						<div className='section-heading  project-image-div'>
+							<h2>Village Savior</h2>
+							<p>Monsters and thieves are about to attack your village. Stave them off as long as you can in order to give the villagers time to escape! Be very careful! If your health falls to 0, you must retreat!</p>
+							<p> HTML & CSS, JavaScript (code is intentionally written without frameworks)</p>
+							{/*Link to youtube video */}
+							<div className='repo-demo-links'>
+								<a className='margin-right' target="_blank" href='https://www.youtube.com/watch?v=kVRCTKB7NRo&feature=youtu.be'><button className='btn btn-primary'> Demo </button></a>
+								<a target="_blank" href='https://github.com/rishikarri/village-savior'><button className='btn btn-primary'>Repository</button></a>
+							</div>
+						</div>
+					</div>
+
+					<div className='col-xs-12 col-sm-5'>						
+						<img className='project-image margin-above' src='../../Images/Projects-Page/survivor-screenshot.png' />
 					</div>
 
 
