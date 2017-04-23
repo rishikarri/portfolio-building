d277e087627900a80746949c3db1721ce2a9f808
Rishi Karri <rishikarri1@gmail.com>
2017-04-23 15:25:00 -0400

    about section updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 // This section contains the technologies I know in addition to a slight blurb about me and the education I have received at DigitalCrafts and UW
+
 class About extends Component{
 
 
