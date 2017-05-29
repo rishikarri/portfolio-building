cad2d3205fffd1165fb55cbc6065da122ff26c21
Rishi Karri <rishikarri1@gmail.com>
2017-05-29 12:56:08 -0400

    updated reading list

--- src/Components/Interests.js
+++ src/Components/Interests.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 // This section holds my interests in addition to whatever i'm reading at the moment 
 // Interests have a spin attached to them - reading section has author, date completed, book and link to goodreads
+// need to add latest harry potter
+
 class Interests extends Component{
 
 	
