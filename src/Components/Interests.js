faf6daf8914cc088e06818bc4c4222f209350a01
Rishi Karri <rishikarri1@gmail.com>
2017-08-25 13:20:46 -0400

    update to interests page

--- src/Components/Interests.js
+++ src/Components/Interests.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 // This section holds my interests in addition to whatever i'm reading at the moment 
 // Interests have a spin attached to them - reading section has author, date completed, book and link to goodreads
-// need to add reluctant fundamentalist to list 
-
 
 
 class Interests extends Component{
