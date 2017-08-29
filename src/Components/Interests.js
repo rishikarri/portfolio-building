a7d3efc818cd2ba9e4ad79f6ba370c3099ae8b14
Rishi Karri <rishikarri1@gmail.com>
2017-08-29 16:27:24 -0400

    updates to reading list

--- src/Components/Interests.js
+++ src/Components/Interests.js
@@ -92,9 +92,9 @@ class Interests extends Component{
 										</tr>
 
 										<tr className='taller-table-rows'>					
-											<td className='data-entry'><a href='https://www.goodreads.com/book/show/88815.The_Reluctant_Fundamentalist' target="_blank">Moth Smoke</a></td>
+											<td className='data-entry'><a href='https://www.goodreads.com/book/show/440777.Moth_Smoke' target="_blank">Moth Smoke</a></td>
 											<td className='data-entry'>Mohsin Hamid</td>										
-											<td className='data-entry'>8/19/2017</td>										
+											<td className='data-entry'>Currently Reading</td>										
 										</tr>
 
 										<tr className='taller-table-rows'>					
