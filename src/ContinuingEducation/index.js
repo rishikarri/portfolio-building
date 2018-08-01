baf652673f870ecf12e686f74cc3195b31a4cee4
Rishi Karri <rishikarri1@gmail.com>
2018-07-31 21:56:55 -0400

    updated continuing education

--- src/ContinuingEducation/index.js
+++ src/ContinuingEducation/index.js
@@ -17,16 +17,18 @@ class ContinuingEducation extends Component{
 		                <li style={{fontSize: 14, color: 'black'}}><a target="_blank" href="http://online-learning.harvard.edu/course/cs50-introduction-computer-science">CS50 - Introduction to Computer Science </a>- 0% Complete </li>
 					</p>*/}
 
-					<div className="col-xs-12" style={{display: 'flex'}}><a target="_blank" href="https://www.udemy.com/">Udemy</a></div>
+					<div className="col-xs-12" style={{display: 'flex'}}><a target="_blank" href="https://www.udemy.com/">Udemy Courses</a></div>
 					<p className='col-xs-12' style={{marginLeft: 20, textAlign: 'left', }}> 
-		                <li style={{ fontSize: 14, color: 'black'}}>Advanced React & Redux - Stephen Grider - <em>29% Complete</em></li>
-		                <li style={{ fontSize: 14, color: 'black'}}>React Native and Redux - Stephen Grider - <em>75% Complete</em></li> 
-		                <li style={{ fontSize: 14, color: 'black',}}>NodeJS - Anthony Alicea - <em>100% Complete</em></li>
+		                <li style={{ fontSize: 14, color: 'black'}}>Advanced React & Redux - Stephen Grider</li>
+		                <li style={{ fontSize: 14, color: 'black'}}>React Native and Redux - Stephen Grider</li> 
+		                <li style={{ fontSize: 14, color: 'black',}}>NodeJS - Anthony Alicea</li>
 
 					</p>
 
-					<div className="col-xs-12" style={{display: 'flex'}}><span style={{color: '#337ab7'}}>Tech Books Completed</span></div>
-					<p className='col-xs-12' style={{marginLeft: 20, textAlign: 'left', }}> 						
+					<div className="col-xs-12" style={{display: 'flex'}}><span style={{color: '#337ab7'}}>Tech Books Read</span></div>
+					<p className='col-xs-12' style={{marginLeft: 20, textAlign: 'left', }}>
+						<li style={{ fontSize: 14, color: 'black'}}>The Pragmatic Programmer - Andy Hunt and Dave Thomas</li> 						
+						<li style={{ fontSize: 14, color: 'black'}}>The C Programming Language - Brian Kernighan and Dennis Ritchie</li> 						
 		                <li style={{ fontSize: 14, color: 'black'}}>A Smarter Way to Learn JavaScript - Mark Myers</li>
 		                <li style={{ fontSize: 14, color: 'black',}}>A Smarter Way to Learn HTML & CSS - Mark Myers</li>
 		                <li style={{ fontSize: 14, color: 'black'}}>Learn Python the Hard Way - Zed Shaw</li> 
@@ -34,7 +36,7 @@ class ContinuingEducation extends Component{
 
 					<div className="col-xs-12" style={{display: 'flex'}}><span style={{color: '#337ab7'}}>Other</span></div>
 					<p className='col-xs-12' style={{marginLeft: 20, textAlign: 'left', }}> 
-		                <li style={{ fontSize: 14, color: 'black'}}>15 Books Read in 2018 (See Interests Section for Details)</li>
+		                <li style={{ fontSize: 14, color: 'black'}}>19 Books Read in 2018 (See Interests Section for Details)</li>
 		                <li style={{ fontSize: 14, color: 'black'}}>30 Books Read in 2017 (See Interests Section for Details)</li>
 					</p>
 
