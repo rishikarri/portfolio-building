435dcb2f2ce553c295bb2a8bbd0da1cd2645c0ae
Rishi Karri <rishikarri1@gmail.com>
2025-01-30 01:06:45 -0500

    feat: add code repo

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -32,7 +32,7 @@ class Project extends Component {
 									<button className="btn btn-primary"> Demo</button>
 								</a>
 								<a target="_blank" href="https://github.com/rishikarri/village-savior">
-									<button className="btn btn-primary">Repository</button>
+									<button className="btn btn-primary">Code Repository</button>
 								</a>
 							</div>
 						</div>
